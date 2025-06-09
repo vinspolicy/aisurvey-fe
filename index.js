ade8776c1e58a1d9ea58ab114e53e4953fc3f1ad
ichininja <vinayaksemail@gmail.com>
2025-06-09 03:38:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ stopButton.onclick = () => {
 function displayCoreIdeas(ideasJson) {
   try {
     const ideas = JSON.parse(ideasJson);
+    console.log(ideas);
     coreIdeasDiv.innerHTML = "<h3>मुख्य समस्याएँ:</h3>";
     ideas.forEach((idea) => {
       const label = document.createElement("label");
